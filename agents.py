5cae4f104f7007b93fc3c5f33f6c1b66984c4fe8
Yash Shah <yashshah2398@gmail.com>
2019-11-18 22:53:21 +0530

    added KL-UCB

--- agents.py
+++ agents.py
@@ -13,7 +13,11 @@ class Agent(object):
 		raise NotImplementedError
 
 	def run(self, horizon=100):
-		raise NotImplementedError
+		self.rewards = np.zeros(len(A))
+		self.n_pulled = np.zeros(len(A))
+		for t in range(horizon):
+			self._step(t)
+		return self.rewards.sum()
 
 class UCBAgent(Agent):
 	def __init__(self, G, A):
@@ -35,9 +39,50 @@ class UCBAgent(Agent):
 			self.n_pulled[a] += 1
 			self.rewards[a] += reward
 
-	def run(self, horizon=100):
-		self.rewards = np.zeros(len(A))
-		self.n_pulled = np.zeros(len(A))
-		for t in range(horizon):
-			self._step(t)
-		return self.rewards.sum()
+class KL_UCBAgent(Agent):
+	def __init__(self, G, A):
+		super(KL_UCBAgent, self).__init__(G, A)
+
+	def _objective(self, t):
+		DELTA = 1e-4
+		EPSILON = 1e-4
+
+		p_hat = self.rewards / self.n_pulled
+		t1 = np.array([0. if p_hat[i] == 0. or p_hat[i] == 1. \
+			else p_hat[i] * np.log(p_hat[i]) + (1. - p_hat[i]) * np.log(1. - p_hat[i]) \
+			for i in range(len(self.actions))])
+		t2 = (np.log(t) + 3. * np.log(np.log(t))) / self.n_pulled
+		constant = t1 - t2
+
+		q = np.minimum(np.maximum((1. + p_hat) / 2., p_hat + DELTA), 1.-DELTA)
+		fn_val = constant - p_hat*np.log(q) - (1. - p_hat)*np.log(1. - q)
+		
+		while not all(np.logical_or(np.abs(fn_val) < EPSILON, q >= 1.-DELTA)):
+			fn_q = (q - p_hat) / (q * (1. - q))
+			q -= fn_val / fn_q
+			q = np.minimum(np.maximum(q, p_hat + DELTA), 1.-DELTA)
+			fn_val = constant - p_hat*np.log(q) - (1. - p_hat)*np.log(1. - q)
+
+		return q
+
+	def _step(self, t):
+		if len(self.actions) > 2 and t < len(self.actions):
+			# initialize by pulling all arms once
+			assignments = self.graph.intervention(self.actions[t])
+			reward = assignments[len(assignments)]
+			self.n_pulled[t] += 1
+			self.rewards[t] += reward
+		elif len(self.actions) <= 2 and t < 2*len(self.actions):
+			# initialize by pulling all arms twice
+			# done to avoid ln(t)+3*ln(ln(t)) from becoming negative
+			assignments = self.graph.intervention(self.actions[t%len(self.actions)])
+			reward = assignments[len(assignments)]
+			self.n_pulled[t%len(self.actions)] += 1
+			self.rewards[t%len(self.actions)] += reward
+		else:
+			ucb = self._objective(t)
+			arm = np.argmax(ucb)
+			assignments = self.graph.intervention(self.actions[arm])
+			reward = assignments[len(assignments)]
+			self.n_pulled[arm] += 1
+			self.rewards[arm] += reward
