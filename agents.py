5f4a833c6cfef8e25f541b650c41ad38179a97ca
Yash Shah <yashshah2398@gmail.com>
2019-11-18 22:27:35 +0530

    added UCB

--- agents.py
+++ agents.py
@@ -9,7 +9,7 @@ class Agent(object):
 		self.graph = G
 		self.actions = A
 
-	def _step(self):
+	def _step(self, t=0):
 		raise NotImplementedError
 
 	def run(self, horizon=100):
@@ -18,4 +18,26 @@ class Agent(object):
 class UCBAgent(Agent):
 	def __init__(self, G, A):
 		super(UCBAgent, self).__init__(G, A)
-		
+
+	def _step(self, t):
+		if t < len(self.actions):
+			# sample each arm/action once
+			assignments = self.graph.intervention(self.actions[t])
+			reward = assignments[len(assignments)]
+			self.n_pulled[t] += 1
+			self.rewards[t] += reward
+		else:
+			# follow UCB algorithm
+			ucb = self.rewards/self.n_pulled + np.sqrt(2*np.log(t)/self.n_pulled)
+			a = np.argmax(ucb)
+			assignments = self.graph.intervention(self.actions[a])
+			reward = assignments[len(assignments)]
+			self.n_pulled[a] += 1
+			self.rewards[a] += reward
+
+	def run(self, horizon=100):
+		self.rewards = np.zeros(len(A))
+		self.n_pulled = np.zeros(len(A))
+		for t in range(horizon):
+			self._step(t)
+		return self.rewards.sum()
